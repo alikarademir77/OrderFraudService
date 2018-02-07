895e5b3c910e336804f81f2b6645dd319e02613d
Keval Shah <kevshah@bestbuycanada.ca>
2018-02-07 11:11:23 -0800

    Removed unnecessary comments

--- src/main/java/com/bbyc/orders/Application.java
+++ src/main/java/com/bbyc/orders/Application.java
@@ -3,12 +3,6 @@ package com.bbyc.orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Written by Keval
- *
- */
-
-
 @SpringBootApplication
 public class Application
 {
