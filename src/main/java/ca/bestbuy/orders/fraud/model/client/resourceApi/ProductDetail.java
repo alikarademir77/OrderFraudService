e1539cca998d9d9131f5c84ff030fd042f8b1a22
junliu <junliu@bestbuycanada.com>
2018-03-22 15:10:48 -0700

    OR-556 Added annotation for getter setter

--- src/main/java/ca/bestbuy/orders/fraud/model/client/resourceApi/ProductDetail.java
+++ src/main/java/ca/bestbuy/orders/fraud/model/client/resourceApi/ProductDetail.java
@@ -1,5 +1,9 @@
 package ca.bestbuy.orders.fraud.model.client.resourceapi;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class ProductDetail {
     private String sku;
     private String department;
