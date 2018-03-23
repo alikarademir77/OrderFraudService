8779e0cb279ceade11704ca29f7da6531e65fb8d
Ali Karademir <akaradem@bestbuycanada.ca>
2018-03-23 15:05:43 -0700

    Update FraudResult top be more Order Fraud Service friendly

--- src/main/java/ca/bestbuy/orders/fraud/model/internal/FraudResult.java
+++ src/main/java/ca/bestbuy/orders/fraud/model/internal/FraudResult.java
@@ -1,117 +1,27 @@
 package ca.bestbuy.orders.fraud.model.internal;
 
-import java.util.List;
+import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@Setter
 public class FraudResult {
 
-
-    private String actionCode;
-    private String errorDescription;
-    private String transactionId;
-    private String version;
-    private String crossReference;
-    private String rulesTripped;
-    private String totalScore;
+    private String fraudResponseStatus;
+    private String orderNumber;
+    private long requestVersion;
+    private String totalFraudScore;
     private String recommendationCode;
-    private String remarks;
-    private String reasonCode;
-    private String reasonDescription;
-    private List<String> transactionDetails;
+    private String accertifyUser;
+    private Date accertifyUserActionTime;
 
-
-    public String getActionCode() {
-        return actionCode;
-    }
-
-    public void setActionCode(String actionCode) {
-        this.actionCode = actionCode;
-    }
-
-    public String getErrorDescription() {
-        return errorDescription;
-    }
-
-    public void setErrorDescription(String errorDescription) {
-        this.errorDescription = errorDescription;
-    }
-
-    public String getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(String transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getCrossReference() {
-        return crossReference;
-    }
-
-    public void setCrossReference(String crossReference) {
-        this.crossReference = crossReference;
-    }
-
-    public String getRulesTripped() {
-        return rulesTripped;
-    }
-
-    public void setRulesTripped(String rulesTripped) {
-        this.rulesTripped = rulesTripped;
-    }
-
-    public String getTotalScore() {
-        return totalScore;
-    }
-
-    public void setTotalScore(String totalScore) {
-        this.totalScore = totalScore;
-    }
-
-    public String getRecommendationCode() {
-        return recommendationCode;
-    }
-
-    public void setRecommendationCode(String recommendationCode) {
-        this.recommendationCode = recommendationCode;
-    }
-
-    public String getRemarks() {
-        return remarks;
-    }
-
-    public void setRemarks(String remarks) {
-        this.remarks = remarks;
-    }
-
-    public String getReasonCode() {
-        return reasonCode;
-    }
-
-    public void setReasonCode(String reasonCode) {
-        this.reasonCode = reasonCode;
-    }
-
-    public String getReasonDescription() {
-        return reasonDescription;
-    }
-
-    public void setReasonDescription(String reasonDescription) {
-        this.reasonDescription = reasonDescription;
-    }
-
-    public List<String> getTransactionDetails() {
-        return transactionDetails;
-    }
-
-    public void setTransactionDetails(List<String> transactionDetails) {
-        this.transactionDetails = transactionDetails;
+    public FraudResult(){}
+    //TODO: SystemError and BANKDOWN codes will be handled via exceptions returned
+    public enum FraudResponseStatusCodes{
+    	ACCEPTED, PENDING_REVIEW, SOFT_DECLINE, HARD_DECLINE 
     }
 }
