2a9475cd34749645c0413253ac94de91bda8a191
Keval Shah <kevshah@bestbuycanada.ca>
2018-04-06 11:57:42 -0700

    OR-308 - Initial draft of outbound message model defined

---
+++ src/main/java/ca/bestbuy/orders/messaging/model/OutboundMessagingEvent.java
@@ -0,0 +1,57 @@
+package ca.bestbuy.orders.messaging.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import ca.bestbuy.orders.messaging.EventTypes;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+
+@Getter
+@EqualsAndHashCode
+@ToString
+public final class OutboundMessagingEvent {
+
+    private final EventTypes type;
+
+    @JsonFormat(pattern = "dd/mm/yyyy hh:mm:ss", timezone="UTC")
+    private final Date messageCreationDate;
+
+    private final FraudResult result;
+
+
+    @JsonCreator
+    public OutboundMessagingEvent(final EventTypes type, final Date messageCreationDate, final FraudResult result) {
+        this.type = type;
+        this.messageCreationDate = messageCreationDate;
+        this.result = result;
+    }
+
+
+    @Getter
+    @ToString
+    public static class FraudResult {
+
+        private final String status;
+
+        private final long requestVersion;
+
+        private final String totalFraudScore;
+
+        private final String orderNumber;
+
+        @JsonCreator
+        public FraudResult(String status, long requestVersion, String totalFraudScore, String orderNumber) {
+            this.status = status;
+            this.requestVersion = requestVersion;
+            this.totalFraudScore = totalFraudScore;
+            this.orderNumber = orderNumber;
+        }
+
+    }
+
+}
