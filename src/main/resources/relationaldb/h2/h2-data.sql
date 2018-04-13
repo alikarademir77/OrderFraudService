f90fe470ad9fe836d7255f47582cf595575e2267
Keval Shah <kevshah@bestbuycanada.ca>
2018-04-13 11:54:41 -0700

    OR-308 - Created H2 SQL script that adds some records into the database

--- src/main/resources/relationaldb/h2/h2-data.sql
+++ src/main/resources/relationaldb/h2/h2-data.sql
@@ -1,6 +1,15 @@
+-- *************************************************************************************************************
+-- THIS SCRIPT CREATES THE BASE DATA NEEDED TO BE POPULATED IN THE DATABASE
+-- *************************************************************************************************************
+
+
 INSERT INTO ORDER_FRAUD.ID_GENERATOR (GENERATED_NAME, GENERATED_VALUE) VALUES ('FRAUD_RQST_ID', 0);
 INSERT INTO ORDER_FRAUD.ID_GENERATOR (GENERATED_NAME, GENERATED_VALUE) VALUES ('FRAUD_RQST_STATUS_HSTRY_ID', 0);
 INSERT INTO ORDER_FRAUD.ID_GENERATOR (GENERATED_NAME, GENERATED_VALUE) VALUES ('FRAUD_RQST_STATUS_HSTRY_DTL_ID', 0);
+
+-- *************************************************************************************************************
+-- Populate FRAUD_STATUSES with static data (INITIAL_REQUEST, FINAL_DECISION, PENDING_REVIEW, CANCELLED)
+-- *************************************************************************************************************
 
 INSERT INTO ORDER_FRAUD.FRAUD_STATUSES (FRAUD_STATUS_CODE, FRAUD_STATUS_DESCRIPTION, VERSION, CREATE_DATE, CREATE_USER, UPDATE_DATE, UPDATE_USER) 
 VALUES ('INITIAL_REQUEST', 'Initial request received.', 1, SYSDATE, 'ORDER_FRAUD_SCRIPT', SYSDATE, 'ORDER_FRAUD_SCRIPT');
@@ -14,6 +23,10 @@ VALUES ('PENDING_REVIEW', 'Fraud decision is pending manual review.', 1, SYSDATE
 INSERT INTO ORDER_FRAUD.FRAUD_STATUSES (FRAUD_STATUS_CODE, FRAUD_STATUS_DESCRIPTION, VERSION, CREATE_DATE, CREATE_USER, UPDATE_DATE, UPDATE_USER) 
 VALUES ('CANCELLED', 'Fraud assessment is canceled.', 1, SYSDATE, 'ORDER_FRAUD_SCRIPT', SYSDATE, 'ORDER_FRAUD_SCRIPT');
 
+
+-- *************************************************************************************************************
+-- Populate FRAUD_RQST_TYPES with static data (FRAUD_CHECK, ORDER_CANCEL)
+-- *************************************************************************************************************
 
 INSERT INTO ORDER_FRAUD.FRAUD_RQST_TYPES (REQUEST_TYPE_CODE, REQUEST_TYPE_DESCRIPTION, VERSION, CREATE_DATE, CREATE_USER, UPDATE_DATE, UPDATE_USER) 
 VALUES ('FRAUD_CHECK', 'Fraud check request', 1, SYSDATE, 'ORDER_FRAUD_SCRIPT', SYSDATE, 'ORDER_FRAUD_SCRIPT');
