344dd4d781865a8343189918103088e19a3be61b
Singh, Kundan Kumar (Best Buy Canada) <kundsingh@bestbuycanada.ca>
2018-04-16 10:24:04 -0700

    Fraud Request validation

--- src/main/java/ca/bestbuy/orders/fraud/controller/OrderFraudController.java
+++ src/main/java/ca/bestbuy/orders/fraud/controller/OrderFraudController.java
@@ -2,11 +2,20 @@ package ca.bestbuy.orders.fraud.controller;
 
 import ca.bestbuy.orders.fraud.callback.model.swagger.api.FraudResult;
 import ca.bestbuy.orders.fraud.callback.model.swagger.api.FraudResultDetail;
+import ca.bestbuy.orders.fraud.exception.ErrorHandler;
+import ca.bestbuy.orders.fraud.exception.ValidationException;
+import ca.bestbuy.orders.fraud.model.client.resourcesapi.ProductDetail;
+import ca.bestbuy.orders.fraud.model.internal.FraudAssessmentResult;
+import ca.bestbuy.orders.fraud.service.ResourceApiService;
+import com.google.common.base.Enums;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +24,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by kundsing on 2018-04-04.
  */
@@ -22,14 +36,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/fraudchecktransactions")
 @Api(value="orderfraud", description="Operations for retrieving and update fraud status")
+@Slf4j
 public class OrderFraudController {
 
+    @Autowired
+    ResourceApiService resourceApiService;
     @ApiOperation(value = "Update fraud result for pending review order")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successful Operation"),
-            @ApiResponse(code = 400, message = "Bad Request"),
-            @ApiResponse(code = 404, message = "Resource Not Found"),
-            @ApiResponse(code = 500, message = "Internal Server Error")
+            @ApiResponse(code = 400, message = "Bad Request",response = ErrorHandler.ErrorResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error",response = ErrorHandler.ErrorResponse.class)
     })
     @RequestMapping(value = "/{transactionId}/{versionId}", method = RequestMethod.PUT, produces = "application/json")
     public ResponseEntity saveFraudStatus(
@@ -39,7 +55,7 @@ public class OrderFraudController {
             @PathVariable Integer versionId,
             @RequestBody FraudResult fraudResult){
 
-           FraudResultDetail fraudResultDetail = fraudResult.getFraudResultDetail();
+            validateCallBackrequest(fraudResult);
             /** TO DO implementation **/
 
         return new ResponseEntity("Fraud result saved successfully", HttpStatus.OK);
@@ -56,4 +72,32 @@ public class OrderFraudController {
 //
 //        return new ResponseEntity("Fraud result saved successfully", HttpStatus.OK);
 //    }
+
+    private void validateCallBackrequest(FraudResult fraudResult) throws ValidationException {
+
+        String exceptionMessage = "";
+        if (fraudResult == null){
+            exceptionMessage = "The request body is null or empty";
+        } else if (StringUtils.isBlank(fraudResult.getUpdateUser())){
+            exceptionMessage = "User name in request is null or empty";
+        } else if (fraudResult.getFraudResultDetail() == null){
+            exceptionMessage = "Result detail in fruad call is null";
+        } else if (!decisionMadeResponseStatusCodes().contains(fraudResult.getFraudResultDetail().getActionCode().getValue())) {
+            exceptionMessage = "Action code in fraud call back result is having invalid action code ";
+        }
+
+        if (StringUtils.isNotBlank(exceptionMessage)) {
+            log.error("Validation failed on fraud call back request - {}", exceptionMessage);
+            throw new ValidationException(exceptionMessage);
+        }
+
+    }
+
+    private List<FraudAssessmentResult.FraudResponseStatusCodes> decisionMadeResponseStatusCodes(){
+        return Arrays.asList(new FraudAssessmentResult.FraudResponseStatusCodes[]{
+                FraudAssessmentResult.FraudResponseStatusCodes.ACCEPTED,
+                FraudAssessmentResult.FraudResponseStatusCodes.HARD_DECLINE,
+                FraudAssessmentResult.FraudResponseStatusCodes.SOFT_DECLINE
+        });
+    }
 }
